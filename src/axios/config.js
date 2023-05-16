0c2fe3c87da7478dd9f2343f6d294f81adea1d41
AinaraLopezOrti <ainara@codepremium.es>
2023-05-16 16:37:14 +0200

    provider

--- src/axios/config.js
+++ src/axios/config.js
@@ -3,5 +3,6 @@ export const BASE_URL = 'https://data.tramitgo.com/api/';
 export const API_URL = {
     INVOICE_TO_PAY: 'invoice-to-pay/',
     INVOICE_TO_EMIT: 'invoice-to-emit/',
-    EXPENSE_TICKET: 'expenses/'
+    EXPENSE_TICKET: 'expenses/',
+    PROVIDER: 'providers/',
   };
--- src/pages/suppliers/services.js
+++ src/pages/suppliers/services.js
@@ -1,19 +1,62 @@
-import axios from 'axios';
 
-export const getProviders = async () => {
-    try {
-        const response = await axios.get( 'https://data.tramitgo.com/api/providers/', {
-            headers: { 
-                'accept': 'application/json', 
-                'Authorization': 'Token f787712d52229f4ee1744079b33a6f5c5a93e38e', 
-            },
-            params: {
-                limit: 500,
-                offset: 0
-            },
-        });
-        return response.data;
-      } catch (error) {
-        return console.log('error');
-      }
-  };
+
+  import axios from 'axios';
+  import {
+      API_URL, BASE_URL
+    } from '../../axios/config';
+  
+  export const getProviders = async (token) => {
+      try {
+          const response = await axios.get(BASE_URL + API_URL.PROVIDER, {
+              headers: { 
+                  'accept': 'application/json', 
+                  'Authorization': `Token ${token}`
+              },
+              params: {
+                  limit: 500,
+                  offset: 0
+              },
+          });
+          return response.data;
+        } catch (error) {
+          return console.log('error');
+        }
+    };
+  
+  export const deleteProvider = async (uuid, token) => {
+      try {
+          const response = await axios.delete( BASE_URL + API_URL.PROVIDER + uuid, {
+              headers: { 
+                  'accept': 'application/json', 
+                  'Authorization': `Token ${token}`
+              },
+              params: {
+                  limit: 500,
+                  offset: 0
+              },
+          });
+          return response.data;
+        } catch (error) {
+          return console.log(error);
+        }
+    };
+  
+  
+    export const patchProvider = async (uuid, data, token) => {
+      console.log(token)
+      try {
+          const response = await axios.patch( BASE_URL + API_URL.PROVIDER + uuid, data, {
+              headers: { 
+                  'accept': 'application/json', 
+                  'Authorization': `Token ${token}`
+              },
+              params: {
+                  limit: 500,
+                  offset: 0
+              },
+          });
+          return response.data;
+        } catch (error) {
+          return console.log(error);
+        }
+    };
