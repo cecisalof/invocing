b4a9a48ea15e27814e91a1e5408b8a46652b66cf
AinaraLopezOrti <ainara@codepremium.es>
2023-06-21 10:27:40 +0200

    fix yarn lint

--- src/pages/customHeader.jsx
+++ src/pages/customHeader.jsx
@@ -1,14 +1,11 @@
-import React, { useState} from 'react';
-import './general-style.css'
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import './general-style.css';
 
-export default ({ displayName, props }) => {
-
+const CustomHeader = ({ displayName, props }) => {
   const [searchTerm, setSearchTerm] = useState('');
-  const [originalData, setOriginalData] = useState(null); // variable de estado para los datos originales
+  const [originalData, setOriginalData] = useState(null);
 
-  
-
-  
   const onSearch = (e) => {
     if (e.key === 'Enter') {
       console.log(searchTerm);
@@ -16,51 +13,64 @@ export default ({ displayName, props }) => {
       if (originalData === null) {
         setOriginalData([...tableData]);
       }
-  
+
       const columnParts = props.column.colId.split('.');
       let columnData = tableData;
-  
+
       for (let part of columnParts) {
-        columnData = columnData.map(row => row[part]);
+        columnData = columnData.map((row) => row[part]);
       }
-  
+
       if (searchTerm === '') {
         props.api.setRowData(originalData);
       } else {
-        const filteredData = tableData.filter(row => {
+        const filteredData = tableData.filter((row) => {
           let cellData = row;
           for (let part of columnParts) {
             cellData = cellData[part];
           }
           return cellData.toLowerCase().includes(searchTerm.toLowerCase());
         });
-  
+
         props.api.setRowData(filteredData);
       }
     }
   };
-  
-  
 
   return (
     <div className="custom-header">
       <div style={{ display: 'flex', flexDirection: 'column' }}>
-      
         <span>{displayName}</span>
-        <input type="text" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} placeholder="Buscar" style={{ 
+        <input
+          type="text"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          placeholder="Buscar"
+          style={{
             borderRadius: '100px',
             padding: '5px',
             marginTop: '10px',
             backgroundColor: '#F7FAFF',
             border: 'none',
             outline: 'none',
-            height: '20px', // altura del input
-            width: '120px', // anchura del input
+            height: '20px',
+            width: '120px',
             fontSize: '10px',
-
-          }} onKeyPress={onSearch} />
-          
+          }}
+          onKeyPress={onSearch}
+        />
       </div>
     </div>
   );
 };
+
+CustomHeader.propTypes = {
+  displayName: PropTypes.string.isRequired,
+  props: PropTypes.object.isRequired,
+  api: PropTypes.object.isRequired,
+  column: PropTypes.object.isRequired,
+};
+
+CustomHeader.displayName = 'CustomHeader';
+
+export default CustomHeader;
